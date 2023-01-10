22cae77cec349b39f423d35e9dfa01d48a5c4d08
Umer Ghafoor <umerghaforr@gmail.com>
2023-01-10 17:35:48 +0500

    Document Scanner
    Started the project

--- Source.cpp
+++ Source.cpp
@@ -7,45 +7,82 @@
 using namespace cv;
 using namespace std;
 
-///////////////  WebCam //////////////////////
 
-int main() {
+///////////////  Document Scanner //////////////////////
 
-	VideoCapture cap(0);
-	Mat img;
-	while (1)
+/////////////////////////////////////////////////////////////////////Declarations
+Mat img, imggryblr, imgcany;
+Mat imgcontr;
+vector<Point> imgpoint;
+
+/////////////////////////////////////////////////////////////////Draw Contours
+void drawcont(vector<Point> point, Scalar color)
+{
+	for (int i = 0;i < point.size();i++)
 	{
-		cap.read(img);
-
-		imshow("Image", img);
-		waitKey(10);
+		circle(img, point[i], 30, color, FILLED);
+		//	putText(img, (string)i, )
 	}
-
 }
 
-/////////////////  Importing Video //////////////////////
-//
-//int main() {
-//
-//	string path = "Resources/test_video.mp4";
-//	VideoCapture cap(path);
-//	Mat img;
-//	while (1)
-//	{
-//		cap.read(img);
-//
-//		imshow("Image", img);
-//		waitKey(10);
-//	}
-//	
-//}
+/////////////////////////////////////////////////////////////////////PreProcecing
+Mat preprocess(Mat img)
+{
+	cvtColor(img, imggryblr, COLOR_BGR2GRAY);
+	GaussianBlur(imggryblr, imggryblr, Size(5, 5), 5, 0);
+	Canny(imggryblr, imgcany, 30, 100);
+	Mat k = getStructuringElement(MORPH_RECT, Size(3, 3));
+	dilate(imgcany, imgcany, k);
+	return imgcany;
+}
+/////////////////////////////////////////////////////////////////////Get Contours
+vector<Point> getContours(Mat imgcany) {
 
-/////////////////  Importing Images //////////////////////
-//
-//int main() {
-//
-//	string path = "Resources/test.png";
-//	Mat img = imread(path);
-//	imshow("Image", img);
-//	waitKey(0);
-//}
+	vector<vector<Point>> contours;
+	vector<Vec4i> hirarchy;
+
+	findContours(imgcany, contours, hirarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+	vector<vector<Point>> contpoly(contours.size());
+	vector<Rect> brect(contours.size());
+	vector<Point> bigest;
+	int Maxarea=0;
+
+	for (int i = 0;i < contours.size();i++)
+	{
+		int area = contourArea(contours[i]);
+		if (area > 100)
+		{
+			float peri = arcLength(contours[i], true);
+			approxPolyDP(contours[i], contpoly[i], 0.02 * peri, true);
+			if (contpoly[i].size() == 4 && area < Maxarea)
+			{
+				drawContours(img, contpoly, i, Scalar(255, 0, 255), 5);
+				bigest = { contpoly[i][0],contpoly[i][1], contpoly[i][2], contpoly[i][3] };
+				area = Maxarea;
+				cout << endl << endl << "working" << endl << endl;
+				
+			}
+			
+		}
+	}
+	return bigest;
+}
+
+///////////////////////////////////////////MAIN FUNCTIONS////////////////////////////////////////////////////
+
+void main() {
+
+	string path = "Resources/paper.jpg";
+	Mat img = imread(path);
+	resize(img, img, Size(), 0.5, 0.5);
+
+	imgcany = preprocess(img);
+	imgpoint = getContours(imgcany);
+	drawcont(imgpoint, Scalar(255, 0, 155));
+
+
+	imshow("Image", img);
+	imshow("Image canny", imgcany);
+	waitKey(0);
+}
